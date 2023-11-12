521784c7e40b428f59abea6c4cec11dbc4b8f597
Javier Zaragoza Tejeda <jazarago@c1r7s1.42madrid.com>
2023-11-12 18:38:10 +0100

    afkas

--- ft_print_ptr.c
+++ ft_print_ptr.c
@@ -1,15 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*   ft_print_ptr.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jazarago <jazarago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/08 16:29:38 by jazarago          #+#    #+#             */
-/*   Updated: 2023/11/12 16:58:06 by jazarago         ###   ########.fr       */
+/*   Created: 2023/11/12 16:19:16 by jazarago          #+#    #+#             */
+/*   Updated: 2023/11/12 17:34:00 by jazarago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "printf.h"
 
-ft_print_ptr
+int	ft_printf_ptr(unsigned long ptr)
+{
+	int	i;
+
+	i = 0;
+	if (ptr == 0)
+		return (ft_print_str(NIL));
+	if (ptr < 16)
+		i = i + ft_print_str("0x");
+	if (ptr >= 16)
+		i = i + ft_print_ptr(ptr / 16);
+	i = i + ft_print_hexa(ptr % 16, 0);
+	return (i);
+}
