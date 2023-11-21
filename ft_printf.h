10f9ee5451b398ece90075a22bdd95c2dfb2f31d
Javier Zaragoza Tejeda <jazarago@c1r5s5.42madrid.com>
2023-11-21 11:35:16 +0100

    printf

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: jazarago <jazarago@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/09 11:58:54 by jazarago          #+#    #+#             */
-/*   Updated: 2023/11/19 18:10:37 by jazarago         ###   ########.fr       */
+/*   Updated: 2023/11/21 10:55:05 by jazarago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,5 +23,6 @@ int	ft_print_str(char *str);
 int	ft_print_unsignedint(unsigned int n);
 int	ft_print_ptr(unsigned long ptr);
 int	ft_print_hexa(unsigned int n, int capital);
+int	ft_printf(const char *format, ...);
 
 #endif
